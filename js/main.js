8e1f6b3d25892d7e507f4509143ecd57f1796216
AlexTurner505 <mdabarros505@gmail.com>
2024-03-03 21:35:59 +0000

    Working Demo

--- js/main.js
+++ js/main.js
@@ -76,7 +76,7 @@ function sendEmail(){
         Subject : document.getElementById("subject").value,
         Body : "Name: " + document.getElementById("name").value + 
         "<br>Email: " + document.getElementById("email").value + 
-        "<br>Message: " + document.getElementById("message").value
+        "<br>Message: " + document.getElementById("message").value,
     }).then(
       message => alert(message)
     );
